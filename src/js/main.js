a2bbc33958e86616589824dfdc4efc231bf36ed9
demo <198024038@qq.com>
2017-09-28 11:20:22 +0800

    base

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+Vue.component("task", {
+  template: "<li><slot></slot></li>"
+});
+Vue.component("task-list", {
+  template: `
+  <div>
+  <task v-for='task in tasks'>{{ task.task }}</task>
+  </div>
+  `,
+  data() {
+    return {
+      tasks: [
+        { task: "go to 01", complete: true },
+        { task: "go to 02", complete: false },
+        { task: "go to 03", complete: true },
+        { task: "go to 04", complete: false }
+      ]
+    };
+  }
+});
+new Vue({
+  el: "#root"
+});
