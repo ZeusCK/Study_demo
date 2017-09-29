06bea7ceeab0abfefbefeb916b8d735b31785d2b
demo <198024038@qq.com>
2017-09-29 09:59:40 +0800

    demo 06

--- src/js/main.js
+++ src/js/main.js
@@ -176,7 +176,38 @@ new Vue({
 /**
  * demo 06
  */
-
+/* Vue.component('modal', {
+  template: `
+  <div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">
+        <slot name="header"></slot>
+      </p>
+      <button class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+      <slot>
+        default content here.
+      </slot>
+    </section>
+    <footer class="modal-card-foot">
+      <slot name="footer">
+      </slot>
+    </footer>
+  </div>
+</div>
+  `
+})
 new Vue({
   el: "#root"
-});
+}); */
+
+/**
+ * demo 07
+ */
+
+/**
+ * demo 08
+ */
